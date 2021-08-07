c04024213413d9a7bd332276da41fa66b8f70d09
DorianZhao <tiz137@ucsd.edu>
2021-08-07 18:33:41 +0800

    api

--- api/api.py
+++ api/api.py
@@ -1,8 +1,20 @@
+from flask import Flask, request
 import time
-from flask import Flask
+import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/login',methods=['GET'])
+def user_login():
+    """Check user's password match with username or not"""
+    username = request.args.get('username')
+    password = request.args.get('password')
+    users = get_users(username)
+    if len(users) == 0:
+        return {"error":"User not found."},400
+    result = False
+    if users[0][2] == password:
+        result = True
+    return {"results":result},200
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 import './sign-in.styles.scss';
+
+let username;
 
 class SignIn extends React.Component{
     constructor(props) {
         super(props);
 
         this.state = {
-            email: '',
-            password: ''
+            username: '',
+            password: '',
+            wrong: false
         };
     }
 
-    handleSubmit = event => {
-        event.preventDefault();
+    login(){
+        const url = 'http://127.0.0.1:5000/login';
+        axios.get(url,{
+            params: {
+                username: this.state.username,
+                password: this.state.password
+            },
+        }).then((response) => {
+            console.log(response);
+            if(response['data']['result'] == true){
+              username = this.state.username;
+            } else {
+                this.setState({
+                    wrong: true
+                })
+            }
+        });
+    };
 
-        this.setState({email: '', password: ''});
+    handleSubmit = event => {
+      event.preventDefault()
+      this.login();
     };
 
     handleChange = event => {
         const { value, name } = event.target;
-    
+        console.log(value)
+        console.log(name)
+
         this.setState({ [name]: value });
-      };
+    };
 
     render(){
         return(
@@ -32,14 +57,14 @@ class SignIn extends React.Component{
                 <h1> I already have an account</h1>
                 <span>Sign in with your email and password</span>
 
-                
+
                 <form onSubmit={this.handleSubmit}>
                 <FormInput
-                    name='email'
-                    type='email'
+                    name='username'
+                    type='username'
                     handleChange={this.handleChange}
-                    value={this.state.email}
-                    label='email'
+                    value={this.state.username}
+                    label='username'
                     required
                 />
                 <FormInput
