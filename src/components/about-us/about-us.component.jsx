078840d16e993d62731cceaf04e0c2e39c8e9d2e
DorianZhao <tiz137@ucsd.edu>
2021-08-08 00:38:09 +0800

    style update

--- src/components/about-us/about-us.component.jsx
+++ src/components/about-us/about-us.component.jsx
@@ -2,11 +2,12 @@ import React from 'react' ;
 import './about-us.styles.scss';
 
 const AboutUs = () => (
-    <div className = 'about-us'>
-        <h1 className = 'content'>
-            This is a brief introduction of our project
+    <div className= 'about-us'>
+
+        <h1 className='content-text'>
+            这是一个让公益捐款更加公开透明化的一个平台。在这里，您可以浏览所有需要帮助的对象，并查看他们的需求，决定是否要进行捐款。平台的每一个用户都可以在登录后查看自己曾经捐款帮助过的对象，以及捐款用途。我们将会和各大公益慈善组织合作，整合资源，使您可以在平台上查看您过去和现在所有的捐款记录和资助对象近况。
         </h1>
-    </div>
+  </div>
 );
 
 export default AboutUs
