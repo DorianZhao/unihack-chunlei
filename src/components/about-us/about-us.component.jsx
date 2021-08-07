d7d13540c1766be05b670d8c22a886b57ecb147e
DorianZhao <tiz137@ucsd.edu>
2021-08-07 10:02:55 +0800

    added AboutUs component and router

---
+++ src/components/about-us/about-us.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react' ; 
+
+const AboutUs = () => (
+    <div className = 'content'>
+        <h2 className = 'text'>
+            This is a brief introduction of our project
+        </h2>
+    </div>
+);
+
+export default AboutUs
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component'
 import WebsiteTitle from '../../components/website-title/website-title.component';
+import AboutUs from '../../components/about-us/about-us.component';
 
 const HomePage = () => (
     <div className = 'homepage'>
         <WebsiteTitle />
         <Directory />
+        <AboutUs />
     </div>
 );
 
