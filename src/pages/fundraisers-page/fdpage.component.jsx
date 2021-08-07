a1863e93eaa8c1d9df210d7b3595ebab9eaf219a
DorianZhao <tiz137@ucsd.edu>
2021-08-07 12:29:21 +0800

    folder directory miss fix

--- src/pages/fundraisers-page/fdpage.component.jsx
+++ src/pages/fundraisers-page/fdpage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './fdpage.styles.scss';
 import Directory from '../../components/directory/directory.component'
 import WebsiteTitle from '../../components/website-title/website-title.component';
-import FdPanel from '../../components/fundraisers-panel/fd-panel.component';
+import FdPanel from '../../components/fundraisers-page-comp/fundraisers-panel/fd-panel.component';
 
 const FdPage = () => (
     <div className = 'FdPage'>
