c685cb8ba784a25675196dadfb2f5478578d5b9e
Runheng Lei <rlei5@wisc.edu>
2021-08-07 14:29:59 +0800

    homepage

--- src/components/about-us/about-us.component.jsx
+++ src/components/about-us/about-us.component.jsx
@@ -1,11 +1,18 @@
-import React from 'react' ; 
+import React from 'react'; 
 import './about-us.styles.scss';
+import image1 from './charity.jpg';
 
 const AboutUs = () => (
     <div className = 'content'>
-        <h2 className = 'text'>
-            This is a brief introduction of our project
-        </h2>
+        <h3 className='text'>
+            
+            This website enables people to donate money to children in need, and track the donation info at any time. 
+            Here, you can both be a donor and a donee.
+             
+        </h3>
+        <img src={image1}
+            alt=" Can not display image"
+            class = "center" />
     </div>
 );
 
